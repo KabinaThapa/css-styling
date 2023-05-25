b5cc78241907be8dc93f3d28c22174904d3651ae
Kabina Thapa <thapakabina28@gmail.com>
2023-05-25 01:58:35 -0400

    background image and more CSS styles added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
-
+import logo from './fb.png';
+import logo1 from './insta.png';
+import logo2 from './twit.png';
 import "./App.css";
 
 function App() {
   const headlines = [
     "I am Strata",
-    "a super responsive website(not yet)",
+    "a super responsive website (not yet)",
     "crafted by Kabina",
   ];
   const result = [];
@@ -45,14 +47,29 @@ function App() {
       <div className="flex">
         <div className="intro">
           <h2>{result}</h2>
+          <div class="Logo">
+          <img src={logo} width="20px" height="20px"/>
+          <img src={logo1} width="20px" height="20px"/>
+          <img src={logo2} width="20px" height="20px"/>
         </div>
+        </div>
+        
           <main className="main">
+            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum maxime alias dolore eveniet unde enim, provident 
+              ipsam repudiandae architecto id. Qui eaque expedita aliquid dolorum temporibus saepe reiciendis sapiente totam.</h3>
             <p>
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit,
               excepturi! Dolor laudantium repellendus, deserunt officia,
               adipisci sed accusantium quisquam quos quia possimus modi facere
               quas nostrum cumque ipsa doloribus tempora?
             </p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit,
+              excepturi! Dolor laudantium repellendus, deserunt officia,
+              adipisci sed accusantium quisquam quos quia possimus modi facere
+              quas nostrum cumque ipsa doloribus tempora?
+            </p>
+            <button class="but">Learn More</button>
           </main>
         </div>
       
